04917e223b8a99e333632ddeeba35ef92f70f37e
ryhhill1998 <ryhhill1998@outlook.com>
2024-03-08 11:25:27 +0000

    added basic file input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <nav>
+          <h1>SynthoTalk</h1>
+        </nav>
+      </header>
+
+      <main>
+        <div className="file-upload">
+          <input type="file" />
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default App;
