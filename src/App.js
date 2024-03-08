a2fef6bcbbdb339b187fb612ba635fb27cc2a606
Gabija Mikulyte <gabijamikulyte@Gabijas-MacBook-Pro.local>
2024-03-08 12:29:27 +0000

    Add parsing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import { useEffect, useState } from "react";
+import Papa from 'papaparse';
 import "./App.css";
 
 const App = () => {
   const [file, setFile] = useState(null);
+  const [data, setData] = useState(null);
 
   const uploadFile = (files) => {
     if (files.length) {
       setFile(files[0]);
+      Papa.parse(files[0], {
+        header: true,
+        complete: (results) => {
+          console.log('Parsed results:', results);
+          console.log(JSON.stringify(results.data))
+          setData(results.data);
+        },
+        error: (error) => {
+          console.error('Error parsing CSV:', error);
+        },
+      });
     }
   };
 
